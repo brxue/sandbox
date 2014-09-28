ad8f3adf144807e95ac62371ac0fcba975cda562
brxue <brxue@outlook.com>
2014-09-28 22:26:22 +0800

    Add an example of how to use breakpoint

---
+++ python/pytest/hello_with_trace.py
@@ -0,0 +1,19 @@
+import pytest
+
+pytest.set_trace()
+
+print "Hello World"
+
+def test_f():
+    print "test fucntion"
+    assert 1==0
+
+class TestHello:
+    def test_a(self):
+        print "hello a"
+        assert 1==1
+
+    def b(self):
+        print "hello b"
+
+#pytest.main("-v")
