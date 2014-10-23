23460dfe0b9b76ce4e67e31fc51e8c8212410e0c
brxue <brxue@outlook.com>
2014-10-23 22:09:28 +0800

    Iterator that calling another iterator inside

--- python/generator/neighbor_iterator.py
+++ python/generator/neighbor_iterator.py
@@ -1,11 +1,27 @@
-def neighbor_iterator(size):
+def x_neighbor_iterator(size):
     x = -1
-    for y in range(-1, size*2-1, 1):
+    for y in reversed(range(-1, size*2, 1)):
         yield (x, y)
 
+def y_neighbor_iterator(size):
     y = -1
-    for x in range(0, size*2-1, 1):
+    for x in range(0, size*2, 1):
         yield (x, y)
 
-for (x, y) in neighbor_iterator(4):
+def xy_neighbor_iterator(size):
+    #x = -1
+    #for y in reversed(range(-1, size*2, 1)):
+    #    yield (x, y)
+
+    #y = -1
+    #for x in range(0, size*2, 1):
+    #    yield (x, y)
+
+    for (x, y) in x_neighbor_iterator(size):
+        yield (x, y)
+
+    for (x, y) in y_neighbor_iterator(size):
+        yield (x, y)
+
+for (x, y) in xy_neighbor_iterator(4):
     print x, y
